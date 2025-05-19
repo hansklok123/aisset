12c5b716c24ef8c3150b482b6d6f9e004cd613f1
hansklok123 <gijssmit1994@gmail.com>
2025-05-19 20:24:08 +0200

    Update aisstream.js

--- aisstream.js
+++ aisstream.js
@@ -4,9 +4,82 @@ const path = require("path");
 
 const DATA_PATH = path.join(__dirname, "public", "data", "schepen.json");
 
-let schepen = {}; // { MMSI: { naam, tijd, type, track: [{lat, lon, time}] } }
-
+let schepen = {}; // { MMSI: { naam, tijd, type, type_naam, lengte, track: [{lat, lon, time}] } }
 let typeCounts = {};
+
+// Volledige AIS type mapping
+const SHIP_TYPE_NAMES = {
+  0: "Onbekend",
+  20: "Wisselverkeer",
+  21: "Wisselverkeer",
+  22: "Wisselverkeer",
+  23: "Wisselverkeer",
+  24: "Wisselverkeer",
+  25: "Wisselverkeer",
+  26: "Wisselverkeer",
+  27: "Wisselverkeer",
+  28: "Wisselverkeer",
+  29: "Wisselverkeer",
+  30: "Vissersvaartuig",
+  31: "Sleepboot",
+  32: "Sleepduwcombinatie",
+  33: "Passagiersschip",
+  34: "Vrachtschip",
+  35: "Tanker",
+  36: "Pleziervaartuig",
+  37: "Pleziervaartuig",
+  40: "Hoogwaardig vaartuig",
+  50: "Pilotvaartuig",
+  51: "Sleepboot",
+  52: "Vrachtschip (met gevaarlijke lading)",
+  53: "Sleper",
+  54: "Ontmijner",
+  55: "Patrouillevaartuig",
+  56: "Marineschip",
+  57: "Zeilschip",
+  58: "Pleziervaartuig",
+  59: "Pleziervaartuig",
+  60: "Passagiersschip",
+  61: "Veerboot",
+  62: "Cruiseschip",
+  63: "Jacht",
+  64: "Jacht",
+  65: "Jacht",
+  66: "Jacht",
+  67: "Jacht",
+  68: "Jacht",
+  69: "Jacht",
+  70: "Cargo schip",
+  71: "Cargo schip",
+  72: "Cargo schip",
+  73: "Cargo schip",
+  74: "Cargo schip",
+  75: "Cargo schip",
+  76: "Cargo schip",
+  77: "Cargo schip",
+  78: "Cargo schip",
+  79: "Cargo schip",
+  80: "Tanker",
+  81: "Tanker",
+  82: "Tanker",
+  83: "Tanker",
+  84: "Tanker",
+  85: "Tanker",
+  86: "Tanker",
+  87: "Tanker",
+  88: "Tanker",
+  89: "Tanker",
+  90: "Overig",
+  91: "Overig",
+  92: "Overig",
+  93: "Overig",
+  94: "Overig",
+  95: "Overig",
+  96: "Overig",
+  97: "Overig",
+  98: "Overig",
+  99: "Onbekend"
+};
 
 // Laad schepenlijst uit bestand bij serverstart (indien aanwezig)
 if (fs.existsSync(DATA_PATH)) {
@@ -78,28 +151,39 @@ function startStream() {
 
       const mmsi = msg.MetaData.MMSI;
       const { latitude, longitude, ShipName, Type, ShipType, VesselType, time_utc } = msg.MetaData;
-      let shipType = "";
-      if (msg.MessageType === "ShipStaticData" && msg.Message && msg.Message.ShipStaticData) {
-      shipType = msg.Message.ShipStaticData.Type || "";
-      } else {
-      shipType = Type || ShipType || VesselType || "";
-        }
       
+      // ===== Type & lengte bepalen (ook ShipStaticData uit Message pakken) =====
+      let shipType = "";
+      let length = null;
+      if (msg.MessageType === "ShipStaticData" && msg.Message && msg.Message.ShipStaticData) {
+        shipType = msg.Message.ShipStaticData.Type || "";
+        // Lengte berekenen uit dimension (A+B)
+        if (msg.Message.ShipStaticData.Dimension) {
+          length = (msg.Message.ShipStaticData.Dimension.A || 0) + (msg.Message.ShipStaticData.Dimension.B || 0);
+        }
+      } else {
+        shipType = Type || ShipType || VesselType || "";
+      }
 
-      // Zoek schip of maak nieuw
+      const typeNaam = SHIP_TYPE_NAMES[shipType] || "Onbekend";
+
+      // ====== Bijwerken schip ======
       if (!schepen[mmsi]) {
         schepen[mmsi] = {
           naam: ShipName || "",
           tijd: time_utc || "",
           type: shipType || "",
+          type_naam: typeNaam,
+          lengte: length,
           track: []
         };
       } else {
         schepen[mmsi].naam = ShipName || schepen[mmsi].naam;
         schepen[mmsi].tijd = time_utc || schepen[mmsi].tijd;
+        if (length) schepen[mmsi].lengte = length; // alleen bijwerken als bekend
       }
-      // Type ALTIJD updaten, ook zonder positie
       schepen[mmsi].type = shipType || schepen[mmsi].type;
+      schepen[mmsi].type_naam = typeNaam;
 
       // Alleen positie toevoegen als die aanwezig is
       if (latitude && longitude) {
