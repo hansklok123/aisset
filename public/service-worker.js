6ea1d2ef65a2f438efffa845840e6a476fb4e65e
hansklok123 <gijssmit1994@gmail.com>
2025-07-06 12:09:18 +0200

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch', function(event) {
-  // Hier kun je caching strategieën toevoegen
+self.addEventListener('push', function(e) {
+  const data = e.data.json();
+  const options = {
+    body: data.body,
+    icon: data.icon || '/logo.png',
+    badge: '/logo.png',
+  };
+  e.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
 });
